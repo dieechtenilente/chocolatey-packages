34ff406c4ad3be5407bb97946dd62147ef524d32
U-RWW001\Klaus <dieechtenilente@users.noreply.github.com>
2022-10-28 17:48:51 +0200

    Version 22.1

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,32 @@
+$ErrorActionPreference = 'Stop';
+ 
+$packageName  = 'mobaxterm'
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'https://download.mobatek.net/2212022060563542/MobaXterm_Installer_v22.1.zip'
+$checkSum     = 'd123f72d1b3713b8fc9183f9841949d5a467047684890c78356abebc817fd909'
+$checkSumType = 'SHA256'
+$MsiInstaller = 'MobaXterm_installer_22.1.msi'
+ 
+$packageArgsZip = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  url            = $url
+  url64bit       = $url
+  checksum       = $checkSum
+  checksumType   = $checkSumType
+  checksum64     = $checkSum
+  checksumType64 = $checkSumType
+}
+ 
+Install-ChocolateyZipPackage @packageArgsZip
+ 
+$packageArgsMsi = @{
+  packageName   = $packageName
+  fileType      = 'MSI'
+  file          = Join-Path $toolsDir $MsiInstaller 
+  file64        = Join-Path $toolsDir $MsiInstaller
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+ 
+Install-ChocolateyInstallPackage @packageArgsMsi
