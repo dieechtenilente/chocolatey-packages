e4fe12814fda14865a8eb072c6b6300c46ad7ecc
U-RWW001\Klaus <dieechtenilente@users.noreply.github.com>
2023-01-29 16:06:39 +0100

    Update MobaXTerm v23.0

--- mobaxterm/tools/chocolateyInstall.ps1
+++ mobaxterm/tools/chocolateyInstall.ps1
@@ -2,10 +2,10 @@ $ErrorActionPreference = 'Stop';
  
 $packageName  = 'mobaxterm'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url          = 'https://download.mobatek.net/2232022120824733/MobaXterm_Installer_v22.3.zip'
-$checkSum     = '0f7408be5fec2b48a32a407d6322e380b691c76fbe5ae3ed32abfcdc0b622d9f'
+$url          = 'https://download.mobatek.net/2302023012231703/MobaXterm_Installer_v23.0.zip'
+$checkSum     = '668550319b08dbc690290b13d129d337b3d82383b7def31afaecff4e793b5c1d'
 $checkSumType = 'SHA256'
-$MsiInstaller = 'MobaXterm_installer_22.3.msi'
+$MsiInstaller = 'MobaXterm_installer_23.0.msi'
  
 $packageArgsZip = @{
   packageName    = $packageName
